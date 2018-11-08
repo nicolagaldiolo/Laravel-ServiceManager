aff437ecfe6fdefaa42e75cc051111ffc609167e
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2018-11-08 13:45:54 +0100

    Style renewal reminder template

--- resources/views/renewalsReminder/reminder.blade.php
+++ resources/views/renewalsReminder/reminder.blade.php
@@ -21,114 +21,115 @@
         </div>
     @else
 
-        <!--begin::Form-->
-        <form method="POST" action="{{route('manage-renewals.update', ['customer'=>$customer, 'token'=>$token])}}">
-            @csrf
-            @method('PATCH')
-
-            @foreach($customer->services as $service)
-                <div class="m-portlet">
-                    <div class="m-portlet__head">
-                        <div class="m-portlet__head-caption">
-                            <div class="m-portlet__head-title">
-                        <span class="m-portlet__head-icon">
-                            <i class="flaticon-layers"></i>
-                        </span>
-                                <h3 class="m-portlet__head-text">{{$service->url}}</h3>
-                            </div>
-                        </div>
-                        <div class="m-portlet__head-tools">
-                            <ul class="nav nav-pills nav-pills--brand m-nav-pills--align-right m-nav-pills--btn-pill m-nav-pills--btn-sm" role="tablist">
-                                <li class="nav-item m-tabs__item">
-                            <span class="nav-link m-tabs__link active">
-                                {{$service->serviceType->name}}
-                            </span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="m-portlet__body">
-
-                        <!--Begin::Tab Content-->
-                        <div class="tab-content">
-
-                            <!--begin::tab 1 content-->
-                            <div class="tab-pane active" id="m_widget11_tab1_content">
-
-                                <!--begin::Widget 11-->
-                                <div class="m-widget11">
-                                    <div class="table-responsive">
-
-                                        <!--begin::Table-->
-                                        <table class="table">
-
-                                            <!--begin::Thead-->
-                                            <thead>
-                                            <tr>
-                                                <td class="m-widget11__app">{{__('messages.deadline')}}</td>
-                                                <td class="m-widget11__total m--align-right">{{__('messages.amount')}}</td>
-                                                <td class="m-widget11__label m--align-right" style="width:17%;">{{__('messages.action')}}</td>
-                                            </tr>
-                                            </thead>
-
-                                            <!--end::Thead-->
-
-                                            <!--begin::Tbody-->
-                                            <tbody>
-                                            @foreach($service->renewalsExpiring as $renewal)
-                                                <tr class="renewal-service-row">
-                                                    <td>
-                                                        <span class="m-widget11__title">{{$renewal->deadline->diffForHumans()}}</span>
-                                                        <span class="m-widget11__sub">{{$renewal->deadlineVerbose}}</span>
-                                                    </td>
-                                                    <td class="m--align-right m--font-brand">{{amount_format($renewal->amount)}}</td>
-                                                    <td class="m--align-right">
-                                                        <div class="m-radio-inline">
-                                                            <label class="m-radio">
-                                                                <input class="renew" type="radio" name="tmp_renewal_id[{{$renewal->id}}]" value="{{\App\Enums\RenewalSM::T_renews}}" @if(\App\Enums\RenewalSM::T_renews == old('renewal_id.' . $renewal->id)) checked @endif> {{\App\Enums\RenewalSM::getDescription(\App\Enums\RenewalSM::T_renews)}}
-                                                                <span></span>
-                                                            </label>
-                                                            <label class="m-radio">
-                                                                <input class="suspend" type="radio" name="tmp_renewal_id[{{$renewal->id}}]" value="{{\App\Enums\RenewalSM::T_suspend}}" @if(\App\Enums\RenewalSM::T_suspend == old('renewal_id.' . $renewal->id)) checked @endif> {{\App\Enums\RenewalSM::getDescription(\App\Enums\RenewalSM::T_suspend)}}
-                                                                <span></span>
-                                                            </label>
-
-                                                            <input type="hidden" name="renewal_id[{{$renewal->id}}]" value="{{old('renewal_id.' . $renewal->id)}}" checked>
-                                                        </div>
-
-                                                        @if ($errors->has('renewal_id.' . $renewal->id))
-                                                            <span class="m--margin-top-10 m-badge m-badge--danger m-badge--wide">{{ $errors->first('renewal_id.' . $renewal->id) }}</span>
-                                                        @endif
-                                                    </td>
-                                                </tr>
-
-                                            @endforeach
-
-                                            </tbody>
-
-                                            <!--end::Tbody-->
-                                        </table>
-
-                                        <!--end::Table-->
+        <div class="renewal-reminder-container" style="height: 100%; position: relative; background:green;">
+            <div class="renewal-reminder-content" style="background: red; overflow: auto; position: absolute; left: 0; top: 0; right: 0; bottom: 0; display: flex; height: 100%;">
+                <form method="POST" action="{{route('manage-renewals.update', ['customer'=>$customer, 'token'=>$token])}}" style="width: 100%; margin:auto;">
+                    @csrf
+                    @method('PATCH')
+                    @foreach($customer->services as $service)
+                        <div class="m-portlet">
+                            <div class="m-portlet__head">
+                                <div class="m-portlet__head-caption">
+                                    <div class="m-portlet__head-title">
+                                <span class="m-portlet__head-icon">
+                                    <i class="flaticon-layers"></i>
+                                </span>
+                                        <h3 class="m-portlet__head-text">{{$service->url}}</h3>
                                     </div>
                                 </div>
-
-                                <!--end::Widget 11-->
+                                <div class="m-portlet__head-tools">
+                                    <ul class="nav nav-pills nav-pills--brand m-nav-pills--align-right m-nav-pills--btn-pill m-nav-pills--btn-sm" role="tablist">
+                                        <li class="nav-item m-tabs__item">
+                                    <span class="nav-link m-tabs__link active">
+                                        {{$service->serviceType->name}}
+                                    </span>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
+                            <div class="m-portlet__body">
 
-                            <!--end::tab 1 content-->
+                                <!--Begin::Tab Content-->
+                                <div class="tab-content">
 
+                                    <!--begin::tab 1 content-->
+                                    <div class="tab-pane active" id="m_widget11_tab1_content">
+
+                                        <!--begin::Widget 11-->
+                                        <div class="m-widget11">
+                                            <div class="table-responsive">
+
+                                                <!--begin::Table-->
+                                                <table class="table">
+
+                                                    <!--begin::Thead-->
+                                                    <thead>
+                                                    <tr>
+                                                        <td class="m-widget11__app">{{__('messages.deadline')}}</td>
+                                                        <td class="m-widget11__total m--align-right">{{__('messages.amount')}}</td>
+                                                        <td class="m-widget11__label m--align-right" style="width:17%;">{{__('messages.action')}}</td>
+                                                    </tr>
+                                                    </thead>
+
+                                                    <!--end::Thead-->
+
+                                                    <!--begin::Tbody-->
+                                                    <tbody>
+                                                    @foreach($service->renewalsExpiring as $renewal)
+                                                        <tr class="renewal-service-row">
+                                                            <td>
+                                                                <span class="m-widget11__title">{{$renewal->deadline->diffForHumans()}}</span>
+                                                                <span class="m-widget11__sub">{{$renewal->deadlineVerbose}}</span>
+                                                            </td>
+                                                            <td class="m--align-right m--font-brand">{{amount_format($renewal->amount)}}</td>
+                                                            <td class="m--align-right">
+                                                                <div class="m-radio-inline">
+                                                                    <label class="m-radio">
+                                                                        <input class="renew" type="radio" name="tmp_renewal_id[{{$renewal->id}}]" value="{{\App\Enums\RenewalSM::T_renews}}" @if(\App\Enums\RenewalSM::T_renews == old('renewal_id.' . $renewal->id)) checked @endif> {{\App\Enums\RenewalSM::getDescription(\App\Enums\RenewalSM::T_renews)}}
+                                                                        <span></span>
+                                                                    </label>
+                                                                    <label class="m-radio">
+                                                                        <input class="suspend" type="radio" name="tmp_renewal_id[{{$renewal->id}}]" value="{{\App\Enums\RenewalSM::T_suspend}}" @if(\App\Enums\RenewalSM::T_suspend == old('renewal_id.' . $renewal->id)) checked @endif> {{\App\Enums\RenewalSM::getDescription(\App\Enums\RenewalSM::T_suspend)}}
+                                                                        <span></span>
+                                                                    </label>
+
+                                                                    <input type="hidden" name="renewal_id[{{$renewal->id}}]" value="{{old('renewal_id.' . $renewal->id)}}" checked>
+                                                                </div>
+
+                                                                @if ($errors->has('renewal_id.' . $renewal->id))
+                                                                    <span class="m--margin-top-10 m-badge m-badge--danger m-badge--wide">{{ $errors->first('renewal_id.' . $renewal->id) }}</span>
+                                                                @endif
+                                                            </td>
+                                                        </tr>
+
+                                                    @endforeach
+
+                                                    </tbody>
+
+                                                    <!--end::Tbody-->
+                                                </table>
+
+                                                <!--end::Table-->
+                                            </div>
+                                        </div>
+
+                                        <!--end::Widget 11-->
+                                    </div>
+
+                                    <!--end::tab 1 content-->
+
+                                </div>
+
+                                <!--End::Tab Content-->
+                            </div>
                         </div>
-
-                        <!--End::Tab Content-->
+                    @endforeach
+                    <div class="m--align-right">
+                        <button type="submit" class="openAlertBeforeSubmit btn m-btn--pill btn-brand btn-lg">{{__('messages.confirm')}}</button>
                     </div>
-                </div>
-            @endforeach
-
-            <div class="m--align-right">
-                <button type="submit" class="openAlertBeforeSubmit btn m-btn--pill btn-brand btn-lg">{{__('messages.confirm')}}</button>
+                </form>
             </div>
+        </div>
 
-        </form>
     @endif
 @stop
