1438ac2a0ba8a9995fd2fd26dbd9b877fdd4499d
Chloe <Chloe@Chloe.homenet.telecomitalia.it>
2021-05-01 11:52:20 +0200

    Fix Renewals Enum

--- app/Enums/RenewalSM.php
+++ app/Enums/RenewalSM.php
@@ -7,11 +7,11 @@ use BenSampo\Enum\Enum;
 
 final class RenewalSM extends Enum implements LocalizedEnum
 {
-    const S_to_confirm = 0;
-    const S_to_bill = 1;
-    const S_to_cash = 2;
-    const S_payed = 3;
-    const S_suspended = 4;
+    const S_to_confirm = '0';
+    const S_to_bill = '1';
+    const S_to_cash = '2';
+    const S_payed = '3';
+    const S_suspended = '4';
 
     const T_back_to_bill = 'to_bill';
     const T_back_to_cash = 'to_cash';
